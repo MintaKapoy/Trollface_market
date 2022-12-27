b5e7d4b1c390709922dec6527b2c89d40eaee505
MintaKapoy <117460389+MintaKapoy@users.noreply.github.com>
2022-12-27 18:03:19 +0200

    catalogue category

---
+++ apps/catalogue/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils.safestring import mark_safe
+from imagekit.models import ProcessedImageField
+from mptt.models import MPTTModel, TreeForeignKey
+from django.urls import reverse
+from pilkit.processors import ResizeToFill
+
+from config.settings import MEDIA_ROOT
+
+
+class Category(MPTTModel):
+    name = models.CharField(verbose_name="Назва", max_length=255)
+    slug = models.SlugField(unique=True, verbose_name="Слаг (ЧПУ)")
+    description = models.TextField(verbose_name="Опис", null=True, blank=True)
+    image = ProcessedImageField(verbose_name="Зображеня", upload_to="blog/catalogue/",
+                                processors=[ResizeToFill(600, 400)], null=True, blank=True)
+    parent = TreeForeignKey(to="self", verbose_name="Батько", related_name="child", on_delete=models.CASCADE, null=True, blank=True)
+
+    def image_tag_thumbnail(self):
+        if self.image:
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}' width=70>")
+
+    image_tag_thumbnail.short_description = "Зображення"
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image}'>")
+
+    image_tag.short_description = "Зображення"
+
+    def __str__(self):
+        full_path = [self.name]
+        parent = self.parent
+        while parent is not None:
+            full_path.append(parent.name)
+            parent = parent.parent
+        return "/".join(full_path[::-1])
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
+
