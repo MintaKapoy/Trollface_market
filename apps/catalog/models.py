8c4061977532b6c962face599bc8eccf319ccebe
MintaKapoy <117460389+MintaKapoy@users.noreply.github.com>
2023-01-03 18:18:01 +0200

    product view

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.safestring import mark_safe
-from imagekit.models import ProcessedImageField
+from imagekit.models import ProcessedImageField, ImageSpecField
 from mptt.models import MPTTModel, TreeForeignKey
 from django.urls import reverse
 from pilkit.processors import ResizeToFill
@@ -45,6 +45,39 @@ class Category(MPTTModel):
         verbose_name_plural = "Категорії"
 
 
+class ProductImage(models.Model):
+    image = ProcessedImageField(verbose_name="Зображеня", upload_to="catalogue/product/")
+    image_thumbnail = ImageSpecField(source="image", processors=[ResizeToFill(600, 400)])
+    product = models.ForeignKey(to="Product", on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name="Основне зображення", default=False)
+
+    def image_tag_thumbnail(self):
+        if not self.image_thumbnail:
+            ProductImage.objects.get(id=self.id)
+        return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}' width=70>")
+
+    image_tag_thumbnail.short_description = "Зображення"
+
+    def image_tag(self):
+        if not self.image_thumbnail:
+            ProductImage.objects.get(id=self.id)
+        return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}'>")
+
+    image_tag.short_description = "Зображення"
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if self.is_main:
+            ProductImage.objects.filter(product=self.product).update(is_main=False)
+        super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return ""
+
+    class Meta:
+        verbose_name = "Зображення товару"
+        verbose_name_plural = "Зображення товару"
+
+
 class Product(models.Model):
     name = models.CharField(verbose_name="Назва", max_length=255)
     slug = models.SlugField(unique=True, verbose_name="Слаг")
@@ -54,6 +87,23 @@ class Product(models.Model):
     categories = models.ManyToManyField(to=Category, verbose_name="Категорії", through="ProductCategory", blank=True)
     updated_at = models.DateTimeField(verbose_name="Дата змінення", auto_now=True)
     created_at = models.DateTimeField(verbose_name="Дата ствонення", auto_now_add=True)
+
+    def images(self):
+        return ProductImage.objects.filter(product=self.id)
+
+    def main_image(self):
+        image = ProductImage.objects.filter(product=self.id, is_main=True).first()
+        if not image:
+            image = self.images().first()
+        return image
+
+    def image_tag(self):
+        image = self.main_image()
+        if image:
+            return image.image_tag_thumbnail()
+
+    def get_absolute_url(self):
+        return reverse('product', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -72,6 +122,9 @@ class ProductCategory(models.Model):
         if self.is_main:
             ProductCategory.objects.filter(product=self.product).update(is_main=False)
         super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return ""
 
     class Meta:
         verbose_name = "Категорія товару"
